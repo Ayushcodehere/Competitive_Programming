6c05f1eb0fc02f1c371a7cf82b0992e3bb0f660f
Swoyam Siddharth Nayak <96427746+swoyam2609@users.noreply.github.com>
2022-09-20 19:53:49 +0530

    20th Sep Evening

--- CodeThru0/shashwat-and-magic-jar.cpp
+++ CodeThru0/shashwat-and-magic-jar.cpp
@@ -1,16 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 int solve(int n){
-    if(n==0){
-        return 0;
-    }
     int ans=0;
     while(n>0){
-        while(n%3==0){
-            n/=3;
-        }
         ans+=n%3;
-        n-=n%3;
+        n=n/3;
     }
     return ans;
     
---
+++ OOPs Classes/20thSep.cpp
@@ -0,0 +1,22 @@
+// // Functions
+
+// // Void Functions
+// void myFunc(){
+//     cout<<"Hello Swoyam";
+// }
+
+// //Non Void Functions
+// int marks(){
+//     return 97;
+// }
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int f(int x){
+    return x*x;
+}
+
+int main(){
+    cout<<f(2)<<endl;
+}
