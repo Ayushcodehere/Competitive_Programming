863ee81df8ddcc89761f40a81f59d930a42e7a69
Swoyam Siddharth Nayak <96427746+swoyam2609@users.noreply.github.com>
2022-09-30 21:51:45 +0530

    Completed Competitive Coding for the day

--- Practice Problems/AVGPERM.cpp
+++ Practice Problems/AVGPERM.cpp
@@ -1,8 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-
-    return 0;
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,i,j,count;
+	    cin>>n;
+	    int ans[n];
+	    i=0;
+	    j=n-1;
+	    count=n;
+	    while(j>=i){
+	        if(i==j) ans[i++]=count;
+	        
+	        else{
+	        ans[j--]=count;
+	        count--;
+	        ans[i++]=count;
+	        count--;
+	        }
+	    }
+	   
+	    for(int p=0;p<n;p++){
+	        cout<<ans[p]<<" ";
+	    }
+	    cout<<"\n";
+	}
+	return 0;
 }
